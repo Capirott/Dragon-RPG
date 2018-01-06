6a463f0d65d8ce7cbc40374a535e4c2848926b7e
Erickson Silva <erickson.lucas@live.com>
2018-01-06 20:54:29 -0200

    k

--- Assets/Enemies/Scripts/Projectile.cs
+++ Assets/Enemies/Scripts/Projectile.cs
@@ -6,16 +6,30 @@ public class Projectile : MonoBehaviour {
 
     [HideInInspector]
     public float damageCaused;
+    public float projectileLifeSpan = 2f;
+    float timeCreated = 0f;
 
     public float projectileSpeed;
-    
+
+    private void Start()
+    {
+        timeCreated = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - timeCreated >= projectileLifeSpan)
+            Destroy(gameObject);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        
         IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
         if (damageable != null)
         {
             damageable.TakeDamage(damageCaused);
+            Destroy(gameObject);
         }
     }
 }

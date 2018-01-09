1d36ab127d09ced96e08cac21b87445712d60222
Erickson Silva <erickson.lucas@live.com>
2018-01-09 13:00:28 -0200

    AOE ATTACKS.

---
+++ Assets/_Characters/Special Abilities/Area Effect/AreaEffectBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+namespace RPG.Characters
+{
+    public class AreaEffectBehaviour : MonoBehaviour, ISpecialAbility
+    {
+        AreaEffectConfig config;
+
+        public void Use(AbilityUseParams useParams)
+        {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, config.GetRadius(), Vector3.up, config.GetRadius());
+            foreach (RaycastHit hit in hits)
+            {
+                var damageable = hit.collider.GetComponent<IDamageable>();
+                if (damageable != null)
+                    damageable.TakeDamage(config.GetDamageToEachTarget());
+            }
+        }
+
+        public void SetPowerAttackConfig(AreaEffectConfig config)
+        {
+            this.config = config;
+        }
+
+        public AreaEffectConfig GetPowerAttackConfig()
+        {
+            return config;
+        }
+    }
+}
---
+++ Assets/_Characters/Special Abilities/Area Effect/AreaEffectConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Area Effect"))]
+    public class AreaEffectConfig : SpecialAbility
+    {
+        [Header("Area Effect Specific")]
+        [SerializeField] float radius = 4f;
+        [SerializeField] float damageToEachTarget = 10f;
+
+        public override void AttachComponentTo(GameObject gameObjectToAttachTo)
+        {
+            AreaEffectBehaviour behaviourComponent = gameObjectToAttachTo.AddComponent<AreaEffectBehaviour>();
+            behaviourComponent.SetPowerAttackConfig(this);
+            behaviour = behaviourComponent;
+        }
+
+        public float GetDamageToEachTarget()
+        {
+            return damageToEachTarget;
+        }
+
+        public float GetRadius()
+        {
+            return radius;
+        }
+    }
+}

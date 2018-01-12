115737a4d197798d42e2ea0f2e88bc07152ef6e9
Erickson Silva <erickson.lucas@live.com>
2018-01-12 19:31:35 -0200

    k

---
+++ Assets/_Characters/CharacterMovement.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.CameraUI;
+
+namespace RPG.Characters
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField] float stoppingDistance = 1f;
+        [SerializeField] float moveSpeedMultiplier = 0.7f;
+        [SerializeField] float animationSpeedMultiplier = 1.5f;
+        [SerializeField] float movingTurnSpeed = 360;
+        [SerializeField] float stationaryTurnSpeed = 180;
+        [SerializeField] float moveThreshold = 1f;
+
+        GameObject walkTarget;
+        NavMeshAgent agent;
+        Animator animator;
+        Rigidbody myRigidbody;
+        float turnAmount;
+        float forwardAmount;
+        Vector3 groundNormal;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            myRigidbody = GetComponent<Rigidbody>();
+            agent = GetComponent<NavMeshAgent>();
+            walkTarget = new GameObject("walkTarget");
+            CameraRaycaster cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+            agent.updateRotation = false;
+            agent.updatePosition = true;
+            agent.stoppingDistance = stoppingDistance;
+            myRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+
+        private void Update()
+        {
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                Move(agent.desiredVelocity);
+            } 
+            else
+            {
+                Move(Vector3.zero);
+            }
+        }
+
+        public void Move(Vector3 movement)
+        {
+            SetFowardAndTurn(movement);
+            ApplyExtraTurnRotation();
+            UpdateAnimator();
+        }
+
+        private void SetFowardAndTurn(Vector3 movement)
+        {
+            if (movement.magnitude > moveThreshold)
+            {
+                movement.Normalize();
+            }
+            var localMove = transform.InverseTransformDirection(movement);
+            turnAmount = Mathf.Atan2(localMove.x, localMove.z);
+            forwardAmount = localMove.z;
+        }
+
+        void UpdateAnimator()
+        {
+            animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+            animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+            animator.speed = animationSpeedMultiplier;
+        }
+
+        void ApplyExtraTurnRotation()
+        {
+            float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, forwardAmount);
+            transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+        }
+
+        private void OnMouseOverEnemy(Enemy enemy)
+        {
+            if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
+            {
+                walkTarget.transform.position = enemy.transform.position;
+                agent.SetDestination(enemy.transform.position);
+            }
+        }
+
+        private void OnMouseOverPotentiallyWalkable(Vector3 destination)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                walkTarget.transform.position = destination;
+                agent.SetDestination(destination);
+            }
+        }
+
+        public void OnAnimatorMove()
+        {
+            if (Time.deltaTime > 0)
+            {
+                Vector3 velocity = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+
+                // we preserve the existing y part of the current velocity.
+                velocity.y = myRigidbody.velocity.y;
+                myRigidbody.velocity = velocity;
+            }
+        }
+
+    }
+}
